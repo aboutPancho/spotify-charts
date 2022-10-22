696dbc383cbbcf98e9136d315cf6073f2a12420b
Francisco Aponte <francisco.aponte@claro.com.ar>
2022-10-22 12:26:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.getElementById('jsonTest').addEventListener('click', loadJSON);
+//
+// function loadJSON() {
+//   fetch('streamingHistory.json')
+//   .then(function(res) {
+//     console.log(res);
+//   })
+// }
+
+const spotifyData = fetch('streamingHistory.json');
+
+console.log(spotifyData);
