0e5d66905563f6087ace878e35985dc74d020566
Roman <romanaaav@gmail.com>
2022-10-22 13:33:54 -0300

    Spotify data sorted by artist msPlayed.

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-// document.getElementById('jsonTest').addEventListener('click', loadJSON);
-//
-// function loadJSON() {
-//   fetch('streamingHistory.json')
-//   .then(function(res) {
-//     console.log(res);
-//   })
-// }
+async function fetchDataJSON() {
+    const response = await fetch('streamingHistory.json')
+    const spotifyData = await response.json();
+    return spotifyData;
+}
 
-const spotifyData = fetch('streamingHistory.json');
+let groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
 
-console.log(spotifyData);
+function sumTimePlayed(arr) {
+    return arr.reduce((accumulator, songData) => {
+        return accumulator + songData.msPlayed;
+      }, 0);
+}
+
+function pageOnload() {
+    fetchDataJSON()
+    .then(jsonDataArray=>{
+        return groupBy(jsonDataArray,'artistName')
+    })
+    .then((groupedData=>{
+        const spotifyArtistByTime = [];
+        Object.entries(groupedData).forEach(keyValuePair => {
+            spotifyArtistByTime.push({
+                artistName: keyValuePair[0],
+                songQuantity: keyValuePair[1].length,
+                msPlayed: sumTimePlayed(keyValuePair[1])  
+            })
+        });
+        spotifyArtistByTime.sort((artistData1, artistData2) => (artistData1.msPlayed < artistData2.msPlayed) ? 1 : -1)
+        console.log(spotifyArtistByTime);
+    }));
+}
+
